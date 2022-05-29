8858a0ef38479d245d45ea2d7950b39ded4653ef
lopuma <lopumacp@hotmail.com>
2022-05-29 05:40:43 +0200

    4 de 5 ejercicios

--- cursoJS/Dia 2/calculadora/js/calculadora.js
+++ cursoJS/Dia 2/calculadora/js/calculadora.js
@@ -1,34 +1,36 @@
 ////////////////////////////////////////////////////////////////////////////////
 // REFERENCIAS
 let pantalla = document.querySelector(".pantalla");
+let teclado = document.querySelector(".pantalla");
+let calculadora = document.querySelector(".calculadora");
 let btnSeta = document.querySelector("[data-accion='seta']");
 let btnReset = document.querySelector("[data-accion='reset']");
 let btnBorrar = document.querySelector("[data-accion='borrar']");
 let btnIgual = document.querySelector("[data-accion='igual']");
 let arrBtnTexto = document.querySelectorAll("[data-texto]");
-////////////////////////////////////////////////////////////////////////////////
+let btnExpandir = document.getElementById('expandir');
 
-
-
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 // VARIABLES GLOBALES
-let expresion = "";
+let expresion = "0";
+let cero = "0";
+let igual = "";
+var voltea = false;
+var exp = false;
 ////////////////////////////////////////////////////////////////////////////////
-
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // EVENTOS CLICK PARA LOS BOTONES "NORMALES"
 for(let item of arrBtnTexto) {
 	item.onclick = function(evento) {
+		inicializarExpresion(); 
 		expresion += this.dataset.texto;
 		pantalla.innerHTML = expresion;
-		console.log(evento)
 	}
 	
 	let caracter = item.dataset.texto;
 	Mousetrap.bind(caracter, function(evento){
-		console.log(evento);
+		inicializarExpresion(); 
 		expresion += evento.key;
 		pantalla.innerHTML = expresion;
 	})
@@ -42,33 +44,95 @@ for(let item of arrBtnTexto) {
 // EVENTOS CLICK PARA BOTONES ESPECIALES
 btnSeta.onclick = function() {
 }
+
 btnReset.onclick = function() {
-	pantalla.innerHTML = "0";
-	expresion = "";
-}
-btnBorrar.onclick = function() {
-	expresion = expresion.slice(0,-1);
+	expresion = "0";
 	pantalla.innerHTML = expresion;
+	cero = "0";
+	igual = "";
 }
+
+btnBorrar.onclick = function() {
+	inicializarExpresion();
+	expresion = expresion.slice(0, expresion.length -1);
+	console.log(expresion)
+	if ( expresion.length == 0 ){
+		expresion = '0';
+		cero = "0";
+		igual = "";
+		pantalla.innerHTML = expresion;
+	} else {
+		pantalla.innerHTML = expresion;
+	}
+	console.log(expresion);
+}
+
 btnIgual.onclick = function() {
 	try {
 		var resultado = eval(expresion);
-		pantalla.innerHTML = resultado.toString().substr(0,12);
+		if ( expresion == '0' ){
+			pantalla.innerHTML = "= " +expresion;
+			expresion=resultado;
+		} else {
+			pantalla.innerHTML = "= " +resultado.toString().substr(0,12);
+			igual = "= " +resultado.toString().substr(0,12)
+		}
 	} catch(error) {
-		pantalla.innerHTML = "ERROR";
+		pantalla.innerHTML = "Expresión malformada";
+		pantalla.style.fontSize = "200%"
+		cero = '0';
+		igual = "";
 	}
-	expresion = "";
 }
-////////////////////////////////////////////////////////////////////////////////
 
+btnExpandir.onclick = function() {
+	let btn1Agrupar = document.getElementById("agr1");
+	let btn2Agrupar = document.getElementById("agr2");
+	let btnHistory = document.getElementById("hist");
+	if (exp){
+		btn1Agrupar.parentNode.removeChild(btn1Agrupar);
+		btn2Agrupar.parentNode.removeChild(btn2Agrupar);
+		btnHistory.parentNode.removeChild(btnHistory);
+		exp = false;
+	} else{
+		txt1 = "("
+		txt2 = ")"
+		txt3 = "His"
+		const btn1 = document.createElement("button");
+		const btn2 = document.createElement("button")
+		const btn3 = document.createElement("button")
+		btn1.onclick = function(){
+			inicializarExpresion(); 
+			expresion += this.dataset.texto;
+			pantalla.innerHTML = expresion;
+		}
+		btn2.onclick = function(){
+			inicializarExpresion(); 
+			expresion += this.dataset.texto;
+			pantalla.innerHTML = expresion;
+		}
+		btn1.innerHTML = txt1;
+		btn2.innerHTML = txt2;
+		btn3.innerHTML = txt3;
+		btn1.id = "agr1";
+		btn2.id = "agr2";
+		btn3.id = "hist";
+		btn1.setAttributeNS("(", "data-texto", "(")
+		btn2.setAttributeNS(")", "data-texto", ")")
 
-
+		var parentDiv = btnExpandir.parentNode;
+		parentDiv.insertBefore(btn1, btnExpandir);
+		parentDiv.insertBefore(btn2, btnExpandir);
+		parentDiv.insertBefore(btn3, btnExpandir);
+		exp = true;
+	}
+}
 /////////////////////////////////////////////////////////////////////////
 // EVENTOS DEL TECLADO
 Mousetrap.bind("enter", function() {
 	btnIgual.click();
 })
-Mousetrap.bind("escape", function() {
+Mousetrap.bind("r", function() {
 	btnReset.click();
 })
 Mousetrap.bind("backspace", function() {
@@ -77,6 +141,38 @@ Mousetrap.bind("backspace", function() {
 Mousetrap.bind("s", function() {
 	btnSeta.click();
 })
+Mousetrap.bind("ñ", function() {
+	if (!voltea){
+		calculadora.style.transform = "rotate(180deg)";
+		voltea = true;
+	} else {
+		calculadora.style.transform = "rotate(0deg)";
+		voltea = false;
+	}
+})
+Mousetrap.bind("(", function() {
+	texto = "("
+	inicializarExpresion(); 
+	expresion += texto;
+	pantalla.innerHTML = expresion;
+})
+Mousetrap.bind(")", function() {
+	texto = ")"
+	inicializarExpresion(); 
+	expresion += texto;
+	pantalla.innerHTML = expresion;
+})
+
+function inicializarExpresion() {
+	pantalla.style.fontSize = "300%"
+	valorExpresionInicial = cero.startsWith('0')
+	valorResultadoInicial = igual.startsWith('=')
+	if (valorExpresionInicial || valorResultadoInicial) {
+		expresion = "";
+		cero = "1";
+		igual = "";
+	}
+}
 /////////////////////////////////////////////////////////////////////////
 
 
